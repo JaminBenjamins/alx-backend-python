7c4d314e7ba84da79d97aeca5eea0855c831ee57
JaminBenjamins <jaminbenjamins@gmail.com>
2023-10-05 23:36:41 +0300

    0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+    A module with a function that performs the addition of two integers.
+"""
+def add(a: float, b: float) -> float:
+    """ Addng two numbers """
+    return a + b
