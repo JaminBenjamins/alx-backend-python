52b2ec037221dd4095473084d4dace64ad22c70f
JaminBenjamins <jaminbenjamins@gmail.com>
2023-10-06 01:03:04 +0300

    A function that takes a mix of integers and float and returns the sum

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A function that sums floats and integers
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]] -> float:
+        """ Takes a list of integers and floats and returns 
+        the sum of all digits in the list """
+        return sum(mxd_lst)
