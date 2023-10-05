e13b2974f05530b1b1d7d0b1eb3ddb3dbb561811
JaminBenjamins <jaminbenjamins@gmail.com>
2023-10-06 00:38:18 +0300

    A function that returns concatenated string

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ A function that joins two string literals """
 
+
 def concat(str1: str, str2: str) -> str:
     """ Concatenating two strings """
     return str1 + str2
