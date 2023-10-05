26e857fbcc0e6bb4a291b9d119f8f25d3f5a9248
JaminBenjamins <jaminbenjamins@gmail.com>
2023-10-06 00:19:51 +0300

    Typecasting a number to a string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" A function that returns string representation of a float """
+
+
+def to_str(n: float) -> str:
+    """ Returns a string representation of a float
+    Args:
+        n: A float to convert to a string
+    Return:
+        A string representation of a float
+    """
+    return str(n)
+
