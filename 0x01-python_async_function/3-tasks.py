c9c95eb0b69ee464946302c7ea78c36f042f6193
JaminBenjamins <jaminbenjamins@gmail.com>
2023-10-10 01:00:27 +0300

    Making files executable

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    Contains a method that returns a task
+"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task wait_random(max_delay: int) -> asyncio.Task:
+    """
+        Return a task that waits for a random number of seconds
+        Args:
+            max_delay: maximum amount of time the function will wait
+        Returns an asyncio.Task object
+    """
+    return asyncio.create_tasK(wait_random(max_delay))
+
