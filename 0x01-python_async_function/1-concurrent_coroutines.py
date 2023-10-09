ed24d89447c0c8a19be388974deb0fdfacf73efd
JaminBenjamins <jaminbenjamins@gmail.com>
2023-10-10 01:17:17 +0300

    Correcting method accessing syntax

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -12,4 +12,4 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) ->List[float]:
     """Call a function that displays the delays in an execution"""
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    return [await task fors task in asyncio.as_completed(tasks)]
+    return [await task for task in asyncio.as_completed(tasks)]
