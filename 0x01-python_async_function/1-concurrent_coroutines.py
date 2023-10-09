46605faf2625bbdf47d2ea532e0ba370e0e5467c
JaminBenjamins <jaminbenjamins@gmail.com>
2023-10-10 01:33:01 +0300

    Defining type annotations

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) ->List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Call a function that displays the delays in an execution"""
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
