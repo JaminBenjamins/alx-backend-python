109ab209943d4799798c57a8cdd26f2a43ccc69e
JaminBenjamins <jaminbenjamins@gmail.com>
2023-10-11 15:21:22 +0300

    Using async_generator function

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+""" A function to return 10 random numbers using async comprehension """
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+        async_comprehension - function to return ten random numbers
+        Arguments:
+            no arguments
+        Return:
+            10 random numbers
+    """
+    result = [i async for i in async_generator()]
+    return result
