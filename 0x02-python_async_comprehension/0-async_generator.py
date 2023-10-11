feff8e993b4dc39d06d9e0fcf11e98c693de70ca
JaminBenjamins <jaminbenjamins@gmail.com>
2023-10-11 15:20:01 +0300

    A function generator that displays  value asynchronously

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" A python looping ten times and returning a random number"""
+import random
+import asyncio
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+        async_generator - function to loop 10 times
+        Arguments:
+            None
+        return a random number
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
